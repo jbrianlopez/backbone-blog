d86ebc2de7714bb11fccfce6d5061dc7c4a27ff0
Brian Lopez <jbrianlopez@gmail.com>
2016-11-13 22:57:23 +0800

    added edit update and delete features

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,38 @@ var BlogView = Backbone.View.extend({
   initialize: function () {
     this.template = _.template($('.blogs-list-template').html());
   },
+  events: {
+    'click .edit-blog': 'edit',
+    'click .update-blog': 'update',
+    'click .cancel': 'cancel',
+    'click .delete-blog': 'delete'
+  },
+  edit: function () {
+    $('.edit-blog').hide();
+    $('.delete-blog').hide();
+    this.$('.update-blog').show();
+    this.$('.cancel').show();
+
+    var author = this.$('.author').html();
+    var title = this.$('.title').html();
+    var url = this.$('.url').html();
+
+    this.$('.author').html('<input type="text" class="form-control author-update" value=' + author + '>');
+    this.$('.title').html('<input type="text" class="form-control title-update" value=' + title + '>');
+    this.$('.url').html('<input type="text" class="form-control url-update" value=' + url + '>');
+  },
+  update: function() {
+    this.model.set('author', $('.author-update').val());
+    this.model.set('title', $('.title-update').val());
+    this.model.set('url', $('.url-update').val());
+
+  },
+  cancel: function() {
+    blogsView.render();
+  },
+  delete: function() {
+    this.model.destroy();
+  },
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
@@ -46,7 +78,14 @@ var BlogsView = Backbone.View.extend({
   model: blogs,
   el: $('.blogs-list'),
   initialize: function () {
+    var self = this;
     this.model.on('add', this.render, this);
+    this.model.on('change', function() {
+      setTimeout(function() {
+        self.render();
+      }, 30);
+    }, this);
+    this.model.on('remove', this.render, this)
   },
   render: function () {
     var self = this;
